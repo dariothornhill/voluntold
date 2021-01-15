a2fe99eb1d5ba9401101007e4f3fbf582a821eb4
Dario Thornhill <dario.thornhill@gmail.com>
2021-01-15 19:12:08 +0100

    Flask proof of life

--- voluntold/server.py
+++ voluntold/server.py
@@ -1,9 +1,14 @@
 from flask import Flask
 from voluntold.slack_client import SlackClient
 
+app = Flask(__name__)
+
+@app.route('/')
+def proof_of_life():
+    return 'It lives'
+
 if __name__ == "__main__":
     slack = SlackClient()
     slack.connect()
     slack.client.chat_postMessage(channel='#bot-test', text="Guess who's back!")
-    app.run(debug=True)
-    
+    app.run()
